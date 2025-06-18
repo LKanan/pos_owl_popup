997a080d29f9e2310e73250c4d2fc7c6ccf341a0
LKanan <lucienkanani@gmail.com>
2025-06-18 22:12:55 +0200

    popup d'erreur et de confirmation

--- static/src/js/Button_test.js
+++ static/src/js/Button_test.js
@@ -2,9 +2,33 @@
 import PosComponent from "point_of_sale.PosComponent"
 import ProductScreen from "point_of_sale.ProductScreen"
 import Registries from "point_of_sale.Registries"
+const { useListener } = require("@web/core/utils/hooks");
 
 class ButtonTest extends PosComponent {
     static template = "ButtonTest"
+    setup(){
+        super.setup()
+        useListener("click", this.button_test_click)
+    }
+
+    async button_test_click() {
+        // Les types de popup disponibles sont :
+        // - "ErrorPopup"
+        // - "ConfirmPopup"
+
+        // this.showPopup("ErrorPopup",{
+        //     title: "Error message",
+        //     body: "You have clicked the button successfully!",
+        // })
+
+        const{confirmed} = await this.showPopup("ConfirmPopup",{
+            title: "Error message",
+            body: "You have clicked the button successfully!",
+            confirmText: "OK",
+            cancelText: "Cancel"
+        })
+        console.log("Button clicked! ", confirmed)
+    }
 }
 Registries.Component.add(ButtonTest)
 
